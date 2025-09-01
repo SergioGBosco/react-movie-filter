1d4d8f4e061868aabe33f600173017e11feb3919
SergioGBosco <sergiogibosco@gmail.com>
2025-09-01 18:16:45 +0200

    feat: add useState for return a filtered array, and fix section select for use on Change

--- README.md
+++ README.md
@@ -31,3 +31,5 @@ Aggiungere un campo di ricerca per filtrare i film anche per titolo.
 Creare un sistema per aggiungere nuovi film alla lista tramite un form.
 
 Buon lavoro!
+
+"
--- src/App.jsx
+++ src/App.jsx
@@ -9,14 +9,32 @@ const filmsList = [
 ]
 
 function App() {
+  const [films, setFilms] = useState(filmsList)
+  const [filteredFilms, SetFilteredFilms] = useState(films)
+  // creo lo stato per assegnarlo alla select
+  const [genreSelected, setGenreSelected] = useState("")
 
+
+
+  useEffect(() => {
+    console.log("è cambiata la variabile di stato")
+    const filtered = films.filter((film) => {
+      //pongo la condizione per restituire l'array iniziale in caso di valore "" nella variabile di stato genreSelected 
+      if (genreSelected !== "")
+        return film.genre == genreSelected
+      else
+        return film
+    },)
+    SetFilteredFilms(filtered)
+  }, [genreSelected]);
 
   return (
     <div className="container">
       <div className="row">
         <div className="col-100">
           <div className="filter_slot">
-            <select name="genre-film" id="genre-film" className="genre-film">
+            <select name="genre-film" id="genre-film" className="genre-film" onChange={(e) => setGenreSelected(e.target.value)}>
+              {/* devo inserire la funzione on change  */}
               <option value="">Filtra per genere...</option>
               <option value="Fantascienza">Fantasciernza</option>
               <option value="Thriller">Thriller</option>
@@ -27,7 +45,7 @@ function App() {
         </div>
 
 
-        {filmsList.map((film) => {
+        {filteredFilms.map((film) => {
           return (
             <div className="col-100">
               <div className="post" >
