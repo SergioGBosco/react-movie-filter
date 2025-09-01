f664c4aebb257545dba170bb7e9f8f2c330813a7
SergioGBosco <sergiogibosco@gmail.com>
2025-09-01 16:31:11 +0200

    Feat: add first layout html

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,33 @@
-
+import { useState, useEffect } from "react"
+const filmsList = [
+  { title: 'Inception', genre: 'Fantascienza' },
+  { title: 'Il Padrino', genre: 'Thriller' },
+  { title: 'Titanic', genre: 'Romantico' },
+  { title: 'Batman', genre: 'Azione' },
+  { title: 'Interstellar', genre: 'Fantascienza' },
+  { title: 'Pulp Fiction', genre: 'Thriller' },
+]
 
 function App() {
 
 
   return (
-    <div>esercizio di oggi</div>
+    <div className="container">
+      <div className="row">
+        {filmsList.map((film) => {
+          return (
+            <div className="col-100">
+              <div className="post" >
+                <>
+                  <h2>{film.title}</h2>
+                  <span>{film.genre}</span>
+                </>
+              </div>
+            </div>
+          )
+        })}
+      </div>
+    </div>
   )
 }
 
